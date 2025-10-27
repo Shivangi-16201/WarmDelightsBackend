d196c62a0dec7fc9e6d3fa9f371e6950b2211eb4
Vaibhav <shivangi16201@gmail.com>
2025-10-27 20:20:52 +0530

    12th Commit

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,13 +1,23 @@
 const nodemailer = require('nodemailer');
 
 // Create transporter
+// const transporter = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: process.env.EMAIL_PORT,
+//   secure: false,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  host: "smtp.gmail.com",
+  port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: "warmdelights11@gmail.com",
+    pass: "otac ebyd xqwx fnmq",
   },
 });
 
